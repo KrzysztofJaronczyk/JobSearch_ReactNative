aef3091341b47c07ff339912fadfae1f35369532
KrzysztofJaronczyk <krzysztofjaronczykzyt@gmail.com>
2024-06-24 15:12:20 +0200

    Styling the home page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,31 @@
-import { Text, View } from "react-native";
+import { ScrollView, View, SafeAreaView } from 'react-native'
+import { useState } from 'react'
+import { Stack, useRouter } from 'expo-router'
+import { COLORS, icons, images, SIZES } from '../constants'
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
+const Home = () => {
+	const router = useRouter()
+	return (
+		<SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+			<Stack.Screen
+				options={{
+					headerStyle: { backgroundColor: COLORS.lightWhite },
+					headerShadowVisible: false,
+					headerLeft: () => <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />,
+					headerRight: () => <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />,
+					headerTitle: '',
+				}}
+			/>
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+			<ScrollView showsVerticalScrollIndicator={false}>
+				<View style={{ flex: 1, padding: SIZES.medium }}>
+					<Welcome />
+					<Popularjobs />
+					<Nearbyjobs />
+				</View>
+			</ScrollView>
+		</SafeAreaView>
+	)
 }
+
+export default Home
