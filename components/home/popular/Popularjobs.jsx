83814c3613448817fe282d7bc5a73027b8ba6b3a
KrzysztofJaronczyk <krzysztofjaronczykzyt@gmail.com>
2024-06-25 14:16:57 +0200

    Finish up landing screen ui

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,46 +1,48 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
-import { useRouter } from 'expo-router';
-import styles from './popularjobs.style';
-import { COLORS, SIZES } from '../../../constants';
-import PopularJobCard from '../../common/cards/popular/PopularJobCard';
-import useFetch from '../../../hook/useFetch';
+import React, { useState } from 'react'
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
+import { useRouter } from 'expo-router'
+import styles from './popularjobs.style'
+import { COLORS, SIZES } from '../../../constants'
+import PopularJobCard from '../../common/cards/popular/PopularJobCard'
+import useFetch from '../../../hook/useFetch'
 
 const Popularjobs = () => {
-  const router = useRouter();
+	const router = useRouter()
 
-  const { data, isLoading, error } = useFetch('search', {
-    query: 'React Developer',
-    num_pages: 1,
-  });
+	const { data, isLoading, error } = useFetch('search', {
+		query: 'React Developer',
+		num_pages: 1,
+	})
 
-  // console.log(data);
+	// console.log(data);
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerTitle}>Popular jobs</Text>
-        <TouchableOpacity>
-          <Text style={styles.headerBtn}>Show all</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.cardsContainer}>
-        {isLoading ? (
-          <ActivityIndicator size='large' color={COLORS.primary} />
-        ) : error ? (
-          <Text>Something went wrong...</Text>
-        ) : (
-          <FlatList
-            data={data}
-            renderItem={({ item }) => <PopularJobCard item={item} />}
-            keyExtractor={item => item?.job_id}
-            contentContainerStyle={{ columnGap: SIZES.medium }}
-            horizontal
-          />
-        )}
-      </View>
-    </View>
-  );
-};
+	const [selectedJob, setSelectedJob] = useState()
 
-export default Popularjobs;
+	return (
+		<View style={styles.container}>
+			<View style={styles.header}>
+				<Text style={styles.headerTitle}>Popular jobs</Text>
+				<TouchableOpacity>
+					<Text style={styles.headerBtn}>Show all</Text>
+				</TouchableOpacity>
+			</View>
+			<View style={styles.cardsContainer}>
+				{isLoading ? (
+					<ActivityIndicator size='large' color={COLORS.primary} />
+				) : error ? (
+					<Text>Something went wrong...</Text>
+				) : (
+					<FlatList
+						data={data}
+						renderItem={({ item }) => <PopularJobCard item={item} />}
+						keyExtractor={item => item?.job_id}
+						contentContainerStyle={{ columnGap: SIZES.medium }}
+						horizontal
+					/>
+				)}
+			</View>
+		</View>
+	)
+}
+
+export default Popularjobs
