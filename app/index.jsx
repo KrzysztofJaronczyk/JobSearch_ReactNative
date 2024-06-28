7c7ca6a5d08311a0053b435ec9bfd1b473fe96c3
KrzysztofJaronczyk <krzysztofjaronczykzyt@gmail.com>
2024-06-28 21:24:52 +0200

    Add search functionality

--- app/index.jsx
+++ app/index.jsx
@@ -6,7 +6,7 @@ import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components
 
 const Home = () => {
 	const router = useRouter()
-
+	const [searchTerm, setSearchTerm] = useState('')
 	const handleMenuPress = () => {
 		console.log('Menu button pressed')
 	}
@@ -31,7 +31,15 @@ const Home = () => {
 
 			<ScrollView showsVerticalScrollIndicator={false}>
 				<View style={{ flex: 1, padding: SIZES.medium }}>
-					<Welcome />
+					<Welcome
+						searchTerm={searchTerm}
+						setSearchTerm={setSearchTerm}
+						handleClick={() => {
+							if (searchTerm) {
+								router.push(`/search/${searchTerm}`)
+							}
+						}}
+					/>
 					<Popularjobs />
 					<Nearbyjobs />
 				</View>
